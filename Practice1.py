9c55e974a69d89bf34d2c176444fe11776c14046
G7-Cyber <55956367+G7-Cyber@users.noreply.github.com>
2019-10-29 14:18:48 +0545

    Add files via upload

---
+++ Practice1.py
@@ -0,0 +1,60 @@
+#print("hello Its Govinda Here")
+#name = input("what is your name? ")
+#age = input("age? ")
+
+#print(name +  "is"  +  age +  "year old")
+lst = ["apple", "orange", "mango", "banana", "apple"]
+#print(lst.count("apple"))
+fruit = "i love fruits because fruit is fresh"
+#print(fruit.count("f"))
+#print(lst.index("banana"))
+#print(fruit.index("f"))
+fruits = ["apple","banana", "mango"]
+#print(fruits.count("mango"))
+#print(fruits.index("mango"))
+#qu = "wow welcome week!"
+#ty = qu.index("we")
+#print(ty)
+song = "thinking out loud"
+#print(song.split())
+songs = "govinda                 bist"
+songs.strip()
+#print("my name is govinda")
+bestfruit ='i love apple than mango'
+#print(bestfruit.split())
+color = ["red", "green", "yellow"]
+s = '; '
+#print(s.join(color))
+#print('===='.join(color))
+#iteration
+for name in ["govinda", "ram", "bist", "ktm", "bouddha"]:
+    print("Hello", name ,"welcome to my youtube channel.")
+for video in "every video are related to game like pubg csgo:":
+    print("name")
+    print(video)
+import turtle
+wn = turtle.Screen()
+ram = turtle.Turtle()
+for aColor in ["red", "black", "green", "yellow"]:
+    ram.color(aColor)
+    ram.forward(58)
+    ram.left(90)
+#accumulation
+number = [5,10,15,20,25,30]
+accum = 0
+for w in number:
+    accum = accum + w
+    print(accum)
+
+name ="Govinda"
+accum = ''
+for n in name:
+    accum = accum + n
+    print(accum)
+
+
+
+
+
+
+
